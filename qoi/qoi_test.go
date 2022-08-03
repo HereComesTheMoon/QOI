87594c50fd53dc3f3643c72cc187018827b31e26
Mond <herecomesthemoon@protonmail.com>
2022-08-03 19:44:08 +0200

    Fixed tests.

--- qoi/qoi_test.go
+++ qoi/qoi_test.go
@@ -5,6 +5,9 @@ import (
 	"image/png"
     "os"
     "bufio"
+    "path/filepath"
+    "strings"
+    "image/color"
 )
 
 type VirtualWriter struct {
@@ -18,123 +21,111 @@ func (w *VirtualWriter) Write(p []byte) (n int, err error) {
 }
 
 
-func _TestDecoder(t *testing.T) {
-	path := "../qoi_test_images/"
-	images := []string{
-        "dice",
-		"kodim10",
-		"kodim23",
-		"qoi_logo",
-		"testcard",
-        "testcard_rgba",
-		"wikipedia_008",
-	}
+func TestDecoder(t *testing.T) {
+    files, err := filepath.Glob("testdata/qoi_test_images/*.qoi")
+    if err != nil {
+        t.Fatalf("%v", err)
+    }
 
-	for _, loc := range images {
-		loc_qoi := path + loc + ".qoi"
-		loc_png := path + loc + ".png"
-		test_decoder(loc_qoi, loc_png, *t)
-	}
+    for _, path := range files {
+        t.Logf("Now decoding: %v\n", path)
+        f, err := os.Open(path)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+        defer f.Close()
+
+        given, err := Decode(f)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+
+        f_, err := os.Open(strings.TrimSuffix(path, filepath.Ext(path)) + ".png")
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+        defer f_.Close()
+
+        wanted, err := png.Decode(f_)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+
+        if given.Bounds() != wanted.Bounds() {
+            t.Fatalf("Image bounds don't match up!\n")
+        }
+
+        for x := 0; x < given.Bounds().Max.X; x++ {
+            for y := 0; y < given.Bounds().Max.Y; y++ {
+                if color.NRGBAModel.Convert(wanted.At(x, y)) != given.At(x, y) {
+                    t.Errorf("Here: (%v, %v). Value QOI: %v, Value PNG: %v\n", x, y, given.At(x, y), wanted.At(x, y))
+                }
+            }
+        }
+    }
 }
 
 func TestEncoder(t *testing.T) {
-	path := "../qoi_test_images/"
-	images := []string{
-        "dice",
-        "kodim10",
-        "kodim23",
-        "qoi_logo",
-		"testcard",
-        "testcard_rgba",
-        "wikipedia_008",
-	}
-
-	for _, loc := range images {
-		loc_qoi := path + loc + ".qoi"
-		loc_png := path + loc + ".png"
-        if !test_encoder(loc_qoi, loc_png, *t) {
-            t.FailNow()
-        }
-	}
-}
-
-func test_decoder(loc_qoi, loc_png string, t testing.T) bool {
-	f, err := os.Open(loc_qoi)
-	if err != nil {
-		t.Fatalf("%v.", err)
-	}
-	im_qoi, err := Decode(f)
-	if err != nil {
-		t.Fatalf("%v.", err)
-	}
-
-	f, err = os.Open(loc_png)
-	if err != nil {
-		t.Fatalf("%v.", err)
-	}
-
-	im_png, err := png.Decode(f)
-	if err != nil {
-		t.Fatalf("%v.", err)
-	}
-
-	if im_png.Bounds() != im_qoi.Bounds() {
-		t.Fatalf("Image bounds don't match up!\n")
-	}
-
-	passed := true
-
-	for x := 0; x < im_qoi.Bounds().Max.X; x++ {
-		for y := 0; y < im_qoi.Bounds().Max.Y; y++ {
-			if im_png.At(x, y) != im_qoi.At(x, y) {
-				t.Logf("Here: (%v, %v). Value QOI: %v, Value PNG: %v\n", x, y, im_qoi.At(x, y), im_png.At(x, y))
-				passed = false
-			}
-		}
-	}
-	return passed
-}
-
-func test_encoder(loc_qoi, loc_png string, t testing.T) bool {
-    p("Now testing on %v...\n", loc_png)
-    f, err  := os.Open(loc_png)
+    files, err := filepath.Glob("testdata/qoi_test_images/*.png")
     if err != nil {
-        t.Fatalf("%v.", err)
+        t.Fatalf("%v", err)
     }
 
-    im_png, err := png.Decode(f)
-    if err != nil {
-        t.Fatalf("%v.", err)
-    }
-
-    encoderOutput := &VirtualWriter{make([]byte, 0)}
-    Encode(encoderOutput, im_png)
-
-    p("Now decoding...\n")
-
-    f, err = os.Open(loc_qoi)
-    if err != nil {
-        t.Fatalf("Opening file failed: %v.", err)
-    }
-
-    buff := bufio.NewReader(f)
-
-    for _ = range encoderOutput.d[:14] {
-        _, _ = buff.ReadByte()
-    }
-
-    for k, val := range encoderOutput.d[14:] {
-        wanted, err := buff.ReadByte()
-        //p("Got/wanted: %v == %v\n", val, wanted)
+    for _, path := range files {
+        t.Logf("Now encoding: %v\n", path)
+        f, err := os.Open(path)
         if err != nil {
-            p("Unable to read from %v.\n", loc_qoi)
-            return false
+            t.Fatalf("%v", err)
         }
-        if val != wanted {
-            p("Incorrect value at position %v. Got: %v. Wanted: %v.\n", k, val, wanted)
-            return false
+        defer f.Close()
+
+        im_png, err := png.Decode(f)
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+
+        given := &VirtualWriter{make([]byte, 0)}
+        Encode(given, im_png)
+
+        f_, err := os.Open(strings.TrimSuffix(path, filepath.Ext(path)) + ".qoi")
+        if err != nil {
+            t.Fatalf("%v", err)
+        }
+        defer f_.Close()
+
+
+        wanted := bufio.NewReader(f_)
+
+        for k, val := range given.d[:12] {
+            wanted, err := wanted.ReadByte()
+            if err != nil {
+                t.Fatalf("%v", err)
+            }
+            if val != wanted {
+                t.Fatalf("Incorrect value in header at position %v. Got: %v. Wanted: %v.\n", k, val, wanted)
+            }
+        }
+
+        wanted.ReadByte()
+        wanted.ReadByte()
+        // Considering the official specification, and other implementations, the channel and colorspace values of the header don't need to match exactly.
+        if given.d[12] != 3 && given.d[12] != 4 {
+            t.Fatalf("Incorrect channel number in header. Got: %v. Wanted: 3 or 4.\n", given.d[12])
+        }
+        if given.d[13] > 1 {
+            t.Fatalf("Incorrect colorspace value in header. Got: %v. Wanted: 0 or 1.\n", given.d[13])
+        }
+
+        for k, val := range given.d[14:] {
+            ref, err := wanted.ReadByte()
+            if err != nil {
+                t.Fatalf("%v", err)
+            }
+            if val != ref {
+                t.Errorf("Incorrect value at position %v. Got: %v. Wanted: %v.\n", k, val, wanted)
+            }
         }
     }
-
-    return true
 }
+
+
